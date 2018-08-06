9024c6956dc14001560c35775a98c8592d15f047
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2018-08-06 09:10:58 +0400

    Delete build.bat

--- build.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\1\PycharmProjects\RailwayStationSimulator
-python setup.py build
-XCOPY C:\Users\1\PycharmProjects\RailwayStationSimulator\build  D:\rssim_builds /e /y
-RMDIR C:\Users\1\PycharmProjects\RailwayStationSimulator\build
