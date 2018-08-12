07f7a3a81bde56151de05b17bbb5b7b922a6e2d3
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2018-08-12 08:55:26 +0400

    implemented game save

---
+++ bottom_bar.py
@@ -0,0 +1,16 @@
+import pygame
+
+import config as c
+from game_object import GameObject
+
+
+class BottomBar(GameObject):
+    def __init__(self):
+        super().__init__()
+        # create bottom bar for buttons
+        self.bottom_bar_surface = pygame.Surface((c.screen_resolution[0], c.bottom_bar_height), pygame.SRCALPHA)
+        self.bottom_bar_surface.fill(c.colors.BLACKOPACITY75)
+
+    def draw(self, surface, base_offset):
+        # draw bottom bar for buttons
+        surface.blit(self.bottom_bar_surface, (0, c.screen_resolution[1] - c.bottom_bar_height))
