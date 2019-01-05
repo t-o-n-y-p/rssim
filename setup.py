493719ba2083b62293dc475e4dcc863410a47a3b
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2019-01-05 22:31:00 +0400

    setup script now does not grab user.db

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ executables = [Executable('rssim.py',
                           base='Win32GUI',
                           icon='icon.ico')]
 
-include_files = ['db', 'img', 'icon.ico', 'perfo-bold.ttf']
+include_files = [('db/config.db', 'db/config.db'), ('db/default.db', 'db/default.db'), 'img', 'icon.ico',
+                 'perfo-bold.ttf']
 
 excludes = ['PIL', 'test', 'distutils', 'email', 'html', 'http', 'unittest', 'urllib', 'dbm', 'pydoc_data',
             'xml', 'xmlrpc']
