33cd9db9ffb8de9a03d5b15204726769c9922949
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2019-01-18 23:11:53 +0400

    added logs

--- rssim.py
+++ rssim.py
@@ -58,6 +58,7 @@ class RSSim:
             self.logger.addHandler(logs_handler)
 
         self.logger.setLevel(self.log_level)
+        self.logger.info('START RSSIM.INIT')
         self.logger.debug('DB connection set up successfully')
         gl.glEnable(gl.GL_BLEND)
         gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
@@ -117,73 +118,132 @@ class RSSim:
         self.logger.debug('app activated successfully')
         self.app.on_change_screen_resolution(self.app.settings.model.screen_resolution,
                                              self.app.settings.model.fullscreen_mode)
+        self.logger.debug('screen resolution changed to {}'.format(self.app.settings.model.screen_resolution))
         if self.app.settings.model.fullscreen_mode:
+            self.logger.debug('fullscreen mode is turned on')
             self.app.on_fullscreen_mode_turned_on()
+            self.logger.debug('fullscreen mode was set successfully')
+        else:
+            self.logger.debug('fullscreen mode is turned off')
+
+        self.logger.info('END RSSIM.INIT')
 
         @surface.event
         def on_draw():
+            self.logger.info('START SURFACE.ON_DRAW')
             self.surface.clear()
+            self.logger.debug('surface cleared')
             self.batches['main_batch'].draw()
+            self.logger.debug('main batch is drawn now')
             self.app.on_draw_main_frame()
+            self.logger.debug('main frame is drawn now')
             self.batches['ui_batch'].draw()
+            self.logger.debug('ui batch is drawn now')
+            self.logger.info('END SURFACE.ON_DRAW')
 
         @surface.event
         def on_mouse_press(x, y, button, modifiers):
+            self.logger.info('START SURFACE.ON_MOUSE_PRESS')
             for h in self.app.on_mouse_press_handlers:
+                self.logger.debug('moving to the next handler')
                 h(x, y, button, modifiers)
+
+            self.logger.info('END SURFACE.ON_MOUSE_PRESS')
 
         @surface.event
         def on_mouse_release(x, y, button, modifiers):
+            self.logger.info('START SURFACE.ON_MOUSE_RELEASE')
             for h in self.app.on_mouse_release_handlers:
+                self.logger.debug('moving to the next handler')
                 h(x, y, button, modifiers)
+
+            self.logger.info('END SURFACE.ON_MOUSE_RELEASE')
 
         @surface.event
         def on_mouse_motion(x, y, dx, dy):
+            self.logger.info('START SURFACE.ON_MOUSE_MOTION')
             for h in self.app.on_mouse_motion_handlers:
+                self.logger.debug('moving to the next handler')
                 h(x, y, dx, dy)
+
+            self.logger.info('END SURFACE.ON_MOUSE_MOTION')
 
         @surface.event
         def on_mouse_drag(x, y, dx, dy, button, modifiers):
+            self.logger.info('START SURFACE.ON_MOUSE_DRAG')
             for h in self.app.on_mouse_drag_handlers:
+                self.logger.debug('moving to the next handler')
                 h(x, y, dx, dy, button, modifiers)
+
+            self.logger.info('END SURFACE.ON_MOUSE_DRAG')
 
         @surface.event
         def on_mouse_leave(x, y):
+            self.logger.info('START SURFACE.ON_MOUSE_LEAVE')
             for h in self.app.on_mouse_leave_handlers:
+                self.logger.debug('moving to the next handler')
                 h(x, y)
 
+            self.logger.info('END SURFACE.ON_MOUSE_LEAVE')
+
     def run(self):
+        self.logger.info('START GAME LOOP')
         fps_timer = 0.0
+        self.logger.debug(f'FPS timer: {fps_timer}')
         while True:
             time_1 = perf_counter()
+            self.logger.debug('START FRAME')
             self.surface.dispatch_events()
+            self.logger.debug('all events dispatched')
             self.app.game.on_update_time()
+            self.logger.debug('time updated')
             self.app.on_update_view()
+            self.logger.debug('all views updated')
             self.surface.dispatch_event('on_draw')
             self.surface.flip()
+            self.logger.debug('all stuff is drawn')
+            self.logger.debug('END FRAME')
             time_4 = perf_counter()
             if perf_counter() - fps_timer > 0.2:
                 self.app.on_update_fps(round(float(1/(time_4 - time_1))))
+                self.logger.debug(f'FPS: {self.app.fps.model.fps}')
                 fps_timer = perf_counter()
+                self.logger.debug(f'FPS timer: {fps_timer}')
 
     def check_for_updates(self):
+        self.logger.info('START RSSIM.CHECK_FOR_UPDATES')
         self.user_db_cursor.execute('SELECT * FROM sqlite_master WHERE type = "table" AND tbl_name = "version"')
         if len(self.user_db_cursor.fetchall()) == 0:
+            self.logger.debug('version info not found')
             self.user_db_cursor.execute('CREATE TABLE version (major integer, minor integer, patch integer)')
+            self.logger.debug('version table created')
             self.user_db_cursor.execute('INSERT INTO version VALUES (0, 9, 1)')
+            self.logger.debug('version 0.9.1 set')
             self.user_db_connection.commit()
 
         self.user_db_cursor.execute('SELECT * FROM version')
+        user_db_version = self.user_db_cursor.fetchone()
+        self.logger.debug(f'user DB version: {user_db_version}')
+        self.logger.debug(f'current game version: {current_version}')
         if self.user_db_cursor.fetchone() < current_version:
+            self.logger.debug('upgrading database...')
             for patch in range(2, current_version[2] + 1):
+                self.logger.debug(f'start 0.9.{patch} migration')
                 with open(f'db/patch/09{patch}.sql', 'r') as migration:
                     for line in migration.readlines():
                         self.user_db_cursor.execute(line)
+                        self.logger.debug(f'executed request: {line}')
 
                 self.user_db_connection.commit()
+                self.logger.debug(f'0.9.{patch} migration complete')
+        else:
+            self.logger.debug('user DB version is up to date')
+
+        self.logger.info('END RSSIM.CHECK_FOR_UPDATES')
 
     def on_save_and_commit_log_level(self, log_level):
-        if self.log_level >= 30 > log_level:
+        self.logger.info('START RSSIM.ON_SAVE_AND_COMMIT_LOG_LEVEL')
+        if self.log_level >= 30 > log_level and not self.logger.hasHandlers():
             if not path.exists('logs'):
                 mkdir('logs')
 
@@ -194,10 +254,13 @@ class RSSim:
                                                current_datetime.time().microsecond))
             logs_handler.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
             self.logger.addHandler(logs_handler)
+            self.logger.debug('created log file')
 
         self.logger.setLevel(log_level)
+
         self.log_level = log_level
         self.user_db_cursor.execute('UPDATE log_options SET log_level = ?', (log_level, ))
+        self.logger.info('END RSSIM.ON_SAVE_AND_COMMIT_LOG_LEVEL')
 
 
 def main():
