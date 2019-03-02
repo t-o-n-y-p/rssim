fd6ec81f2df8ea7de43546f279143bafe3124ccf
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2019-03-02 21:13:39 +0400

    fixed exp for saved base schedule and trains

--- db/patch/094.sql
+++ db/patch/094.sql
@@ -2,4 +2,6 @@ CREATE TABLE game_progress_temp (unlocked_tracks integer, level integer, exp int
 INSERT INTO game_progress_temp (unlocked_tracks, level, exp, money, money_target, supported_cars_min, supported_cars_max, unlocked_car_collections) SELECT unlocked_tracks, level, CAST(exp AS integer), money, money_target, supported_cars_min, supported_cars_max, unlocked_car_collections FROM game_progress
 DROP TABLE game_progress
 ALTER TABLE game_progress_temp RENAME TO game_progress
+UPDATE base_schedule SET exp = CAST(exp AS integer)
+UPDATE trains SET exp = CAST(exp AS integer)
 UPDATE version SET major = 0, minor = 9, patch = 4
