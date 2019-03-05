3f44c6ccf54d89108b5bd44de72a320b5896a495
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2019-03-05 12:02:16 +0400

    savings notification class

---
+++ notifications/enough_money_track_notification.py
@@ -0,0 +1,19 @@
+from logging import getLogger
+
+from notifications import Notification
+
+
+class EnoughMoneyTrackNotification(Notification):
+    """
+    Implements Level up notification.
+    It is triggered when user has money target activated and has saved this amount of money.
+    """
+    def __init__(self):
+        """
+        Properties:
+            caption_key                         i18n resource key for system notification caption
+            message_key                         i18n resource key for system notification message
+        """
+        super().__init__(logger=getLogger('root.notification.enough_money_track_notification'))
+        self.caption_key = 'enough_money_track_notification_caption_string'
+        self.message_key = 'enough_money_track_notification_message_string'
