f47dd75b8579c53a0fada2c22b797d6e1d9fe0c9
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2019-04-22 14:34:09 +0400

    implementing fade in animation subclasses

---
+++ ui/fade_animation/fade_in_animation/game_fade_in_animation.py
@@ -0,0 +1,18 @@
+from logging import getLogger
+
+from ui.fade_animation.fade_in_animation import *
+from database import USER_DB_CURSOR
+
+
+class GameFadeInAnimation(FadeInAnimation):
+    def __init__(self, game_controller):
+        super().__init__(animation_object=game_controller, logger=getLogger('root.app.game.fade_in_animation'))
+        self.map_fade_in_animations = []
+
+    @fade_animation_is_not_active
+    def on_activate(self):
+        self.is_activated = True
+        self.current_opacity_chart_index = self.opacity_chart.index(self.animation_object.view.opacity)
+        USER_DB_CURSOR.execute('SELECT map_id FROM graphics')
+        map_id == USER_DB_CURSOR.fetchone()[0]
+        self.map_fade_in_animations[map_id].on_activate()
---
+++ ui/fade_animation/fade_in_animation/map_fade_in_animation.py
@@ -0,0 +1,28 @@
+from logging import getLogger
+
+from ui.fade_animation.fade_in_animation import *
+
+
+class MapFadeInAnimation(FadeInAnimation):
+    def __init__(self, game_controller):
+        super().__init__(animation_object=game_controller, logger=getLogger('root.app.game.map.fade_in_animation'))
+        self.signal_fade_in_animations = []
+        self.railroad_switch_fade_in_animations = []
+        self.crossover_fade_in_animations = []
+        self.train_fade_in_animations = []
+
+    @fade_animation_is_not_active
+    def on_activate(self):
+        self.is_activated = True
+        self.current_opacity_chart_index = self.opacity_chart.index(self.animation_object.view.opacity)
+        for animation in self.signal_fade_in_animations:
+            animation.on_activate()
+
+        for animation in self.railroad_switch_fade_in_animations:
+            animation.on_activate()
+
+        for animation in self.crossover_fade_in_animations:
+            animation.on_activate()
+
+        for animation in self.train_fade_in_animations:
+            animation.on_activate()
