11a9160f6ab17b541c401eefc73e7f36e5ab682d
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2019-05-29 23:58:39 +0400

    made shop stages structure similar to constructor

--- model/shop_model/__init__.py
+++ model/shop_model/__init__.py
@@ -8,9 +8,10 @@ class ShopModel(Model):
         super().__init__(logger=getLogger(f'root.app.game.map.{map_id}.shop.{shop_id}.model'))
         self.map_id = map_id
         self.shop_id = shop_id
-        self.user_db_cursor.execute('''SELECT locked, current_stage, money, internal_shop_time FROM shops 
+        self.user_db_cursor.execute('''SELECT locked, current_stage, shop_storage_money, internal_shop_time FROM shops 
                                        WHERE map_id = ? AND shop_id = ?''', (self.map_id, self.shop_id))
-        self.locked, self.current_stage, self.money, self.internal_shop_time = self.user_db_cursor.fetchone()
+        self.locked, self.current_stage, self.shop_storage_money, self.internal_shop_time \
+            = self.user_db_cursor.fetchone()
         self.locked = bool(self.locked)
         self.config_db_cursor.execute('''SELECT track_required, first_available_shop_stage FROM shops_config
                                          WHERE map_id = ? AND shop_id = ?''', (self.map_id, self.shop_id))
@@ -33,11 +34,14 @@ class ShopModel(Model):
             self.shop_stages_state_matrix[stage_info[0]].extend([stage_progress_config[0], stage_progress_config[1],
                                                                  0, *stage_progress_config[2::]])
 
+        self.user_db_cursor.execute('SELECT money FROM game_progress')
+        self.money = self.user_db_cursor.fetchone()[0]
+
     def on_save_state(self):
-        self.user_db_cursor.execute('''UPDATE shops SET locked = ?, current_stage = ?, money = ?, internal_shop_time = ?
-                                       WHERE map_id = ? AND shop_id = ?''',
-                                    (int(self.locked), self.current_stage, self.money, self.internal_shop_time,
-                                     self.map_id, self.shop_id))
+        self.user_db_cursor.execute('''UPDATE shops SET locked = ?, current_stage = ?, shop_storage_money = ?, 
+                                       internal_shop_time = ? WHERE map_id = ? AND shop_id = ?''',
+                                    (int(self.locked), self.current_stage, self.shop_storage_money,
+                                     self.internal_shop_time, self.map_id, self.shop_id))
         for stage in self.shop_stages_state_matrix:
             self.user_db_cursor.execute('''UPDATE shop_stages SET locked = ?, under_construction = ?, 
                                            construction_time = ?, unlock_condition_from_level = ?, 
@@ -58,6 +62,10 @@ class ShopModel(Model):
                                         )
 
     def on_activate_view(self):
+        for stage_number in self.shop_stages_state_matrix:
+            self.view.on_update_stage_state(self.shop_stages_state_matrix, stage_number)
+
+        self.view.on_update_money(self.money)
         self.view.on_activate()
 
     def on_unlock(self):
@@ -66,6 +74,7 @@ class ShopModel(Model):
     def on_put_under_construction(self, stage_number):
         self.shop_stages_state_matrix[stage_number][UNLOCK_AVAILABLE] = False
         self.shop_stages_state_matrix[stage_number][UNDER_CONSTRUCTION] = True
+        self.view.on_update_stage_state(self.shop_stages_state_matrix, stage_number)
         self.controller.parent_controller.parent_controller\
             .on_pay_money(self.shop_stages_state_matrix[stage_number][PRICE])
 
@@ -73,12 +82,20 @@ class ShopModel(Model):
         if self.current_stage > 0:
             self.internal_shop_time += 1
             if self.internal_shop_time % FRAMES_IN_ONE_HOUR == 0 \
-                    and self.money < self.shop_stages_state_matrix[self.current_stage][STORAGE_CAPACITY]:
-                self.money += min(self.shop_stages_state_matrix[self.current_stage][HOURLY_PROFIT],
-                                  self.shop_stages_state_matrix[self.current_stage][STORAGE_CAPACITY] - self.money)
+                    and self.shop_storage_money < self.shop_stages_state_matrix[self.current_stage][STORAGE_CAPACITY]:
+                self.shop_storage_money += min(self.shop_stages_state_matrix[self.current_stage][HOURLY_PROFIT],
+                                               self.shop_stages_state_matrix[self.current_stage][STORAGE_CAPACITY]
+                                               - self.shop_storage_money)
                 self.view.on_update_storage()
 
     def on_clear_storage(self):
-        self.controller.parent_controller.parent_controller.on_add_money(self.money)
-        self.money = 0
+        self.controller.parent_controller.parent_controller.on_add_money(self.shop_storage_money)
+        self.shop_storage_money = 0
         self.view.on_clear_storage()
+
+    def on_check_shop_unlock_conditions(self, stage_number):
+        if self.shop_stages_state_matrix[stage_number][UNLOCK_CONDITION_FROM_PREVIOUS_STAGE] \
+                and self.shop_stages_state_matrix[stage_number][UNLOCK_CONDITION_FROM_LEVEL]:
+            self.shop_stages_state_matrix[stage_number][UNLOCK_CONDITION_FROM_PREVIOUS_STAGE] = False
+            self.shop_stages_state_matrix[stage_number][UNLOCK_CONDITION_FROM_LEVEL] = False
+            self.shop_stages_state_matrix[stage_number][UNLOCK_AVAILABLE] = True
