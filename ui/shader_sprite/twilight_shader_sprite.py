3b3ac650d500211d9e88dc137fc5321865a445ee
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2019-10-05 14:51:41 +0400

    Revert "refactoring twilight"
    This reverts commit d3ea557b

--- ui/shader_sprite/twilight_shader_sprite.py
+++ ui/shader_sprite/twilight_shader_sprite.py
@@ -13,8 +13,6 @@ class TwilightShaderSprite(ShaderSprite):
         super().__init__(logger=getLogger('root.app.game.map.view.twilight_shader_sprite'), view=view)
         self.shader = from_files_names('shaders/shader.vert', 'shaders/twilight_view/shader.frag')
         self.group = GROUPS['twilight']
-        self.height_map_uniform_name = (c_char * 10)()
-        self.height_map_uniform_name.value = bytes('height_map', 'utf-8')
 
     def get_bottom_edge(self):
         return get_bottom_bar_height(self.screen_resolution) / self.screen_resolution[1] * 2 - 1
@@ -23,12 +21,14 @@ class TwilightShaderSprite(ShaderSprite):
         return 1 - get_top_bar_height(self.screen_resolution) / self.screen_resolution[1] * 2
 
     def set_uniforms(self):
-        glUniform1i(glGetUniformLocation(self.shader.pid, self.height_map_uniform_name), 0)
+        uniform_name = (c_char * 10)()
+        uniform_name.value = bytes('height_map', 'utf-8')
+        glUniform1i(glGetUniformLocation(self.shader.pid, uniform_name), 0)
         glActiveTexture(GL_TEXTURE0)
         glBindTexture(GL_TEXTURE_2D, self.view.height_map.id)
         self.shader.uniforms.base_offset = self.view.base_offset
         self.shader.uniforms.scale = round(1 / self.view.zoom_factor)
         self.shader.uniforms.twilight_opacity = self.view.opacity
-        self.shader.uniforms.sun_phi_radians = self.view.current_sun_phi
-        self.shader.uniforms.sun_theta_radians = self.view.current_sun_theta
-        self.shader.uniforms.sun_brightness = self.view.current_sun_brightness
+        self.shader.uniforms.sun_phi_radians = self.view.sun_phi
+        self.shader.uniforms.sun_theta_radians = self.view.sun_theta
+        self.shader.uniforms.sun_brightness = self.view.sun_brightness
