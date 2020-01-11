33bf33ff81df939259c27ee20e784ab89ddf7964
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2020-01-11 19:55:30 +0300

    removed unused DB columns, bumped version

--- rssim_core/__init__.py
+++ rssim_core/__init__.py
@@ -2,14 +2,14 @@ from typing import Final
 
 
 # --------------------- CONSTANTS ---------------------
-CURRENT_VERSION: Final = (0, 10, 0)                    # current app version
-MIN_UPDATE_COMPATIBLE_VERSION: Final = (0, 10, 0)      # game cannot be updated from version earlier than this
+CURRENT_VERSION: Final = (0, 10, 2)                    # current app version
+MIN_UPDATE_COMPATIBLE_VERSION: Final = (0, 10, 2)      # game cannot be updated from version earlier than this
 REQUIRED_TEXTURE_SIZE: Final = 8192                    # maximum texture resolution presented in the app
 FPS_INTERVAL: Final = 0.2                              # interval between FPS update
 LOG_LEVEL_OFF: Final = 30                              # integer log level high enough to cut off all logs
 LOG_LEVEL_INFO: Final = 20                             # integer log level which includes basic logs
 LOG_LEVEL_DEBUG: Final = 10                            # integer log level which includes all possible logs
-DATABASE_SHA512: Final = '5bb997b909b11ca099cb5dd723fdc518e406edb1c90198c1e01fe0d930334557ba3171dea6d4461ff4784a8cf542ced2659a06ddf3d32ba9791aae8cb832eebc'
+DATABASE_SHA512: Final = 'aeceaa06546349242964c2d5a670a63d361412455ca0119159d1f44654075cc28d98c936fa47e4bdd3a8203bfdd7c200d82aaf4bbb166cd4fea1dd4c15520e76'
 MAXIMUM_DRAW_EVENTS_PER_FRAME: Final = 1
 MAXIMUM_MOUSE_MOTION_EVENTS_PER_FRAME: Final = 1
 MAXIMUM_MOUSE_DRAG_EVENTS_PER_FRAME: Final = 1
