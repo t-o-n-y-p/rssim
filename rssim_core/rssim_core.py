29aef7628b9db5768505acd21456840d1ef16f3f
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2020-02-09 04:31:03 +0300

    Revert "refactoring main loop"
    This reverts commit 6ee80349

--- rssim_core/rssim_core.py
+++ rssim_core/rssim_core.py
@@ -4,7 +4,7 @@ from logging import FileHandler, Formatter, getLogger
 from datetime import datetime
 from hashlib import sha512
 
-from pyglet import gl, clock, app
+from pyglet import gl
 from keyring import get_password
 
 from exceptions import *
@@ -17,34 +17,83 @@ from controller.app_controller import AppController
 @final
 class RSSim:
     def __init__(self):
-        def on_app_update(dt):
-            # increment in-game time
-            self.app.game.on_update_time()
-            # on_update_view() checks if all views content is up-to-date and opacity is correct
-            self.app.on_update_view()
-            self.on_mouse_motion_event_counter = 0
-            self.on_mouse_drag_event_counter = 0
-            self.on_mouse_scroll_event_counter = 0
+        # determine if video adapter supports all game textures, if not - raise specific exception
+        max_texture_size = c_long(0)
+        gl.glGetIntegerv(gl.GL_MAX_TEXTURE_SIZE, max_texture_size)
+        if max_texture_size.value < REQUIRED_TEXTURE_SIZE:
+            raise VideoAdapterNotSupportedException
+
+        # determine if screen resolution meets requirements, if not - raise specific exception
+        if windll.user32.GetSystemMetrics(0) < MIN_RESOLUTION_WIDTH \
+                or windll.user32.GetSystemMetrics(1) < MIN_RESOLUTION_HEIGHT:
+            raise MonitorNotSupportedException
+
+        with open('db/config.db', 'rb') as f1, open('db/default.db', 'rb') as f2:
+            data = (f2.read() + f1.read())[::-1]
+            if sha512(data[::3] + data[1::3] + data[2::3]).hexdigest() != DATABASE_SHA512:
+                raise HackingDetectedException
+
+        with open(path.join(USER_DB_LOCATION, 'user.db'), 'rb') as f:
+            data = f.read()[::-1]
+            if sha512(data[::3] + data[1::3] + data[2::3]).hexdigest() \
+                    != get_password(sha512('user_db'.encode('utf-8')).hexdigest(),
+                                    sha512('user_db'.encode('utf-8')).hexdigest()):
+                raise HackingDetectedException
+
+        # check if game was updated from previous version (0.9.0 and higher are supported)
+        self.on_check_for_updates()
+        # set up the main logger, create log file
+        self.logger = getLogger('root')
+        current_datetime = datetime.now()
+        if not path.exists('logs'):
+            mkdir('logs')
+
+        logs_handler = FileHandler('logs/logs_{0}_{1:0>2}-{2:0>2}-{3:0>2}-{4:0>6}.log'
+                                   .format(str(current_datetime.date()), current_datetime.time().hour,
+                                           current_datetime.time().minute, current_datetime.time().second,
+                                           current_datetime.time().microsecond),
+                                   encoding='utf8')
+        logs_handler.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.logger.addHandler(logs_handler)
+        self.logger.setLevel(LOG_LEVEL_DEBUG)
+        # set blending mode; this is required to correctly draw transparent textures
+        gl.glEnable(gl.GL_BLEND)
+        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+        # create App object
+        self.app = AppController(loader=self)
+        # activate app after it is created
+        self.app.fade_in_animation.on_activate()
+        self.notifications = []
+        self.on_mouse_motion_event_counter = 0
+        self.on_mouse_motion_cached_movement = [0, 0]
+        self.on_mouse_drag_event_counter = 0
+        self.on_mouse_drag_cached_movement = [0, 0]
+        self.on_draw_event_counter = 0
+        self.on_mouse_scroll_event_counter = 0
+        self.on_mouse_scroll_cached_movement = [0, 0]
 
         @WINDOW.event
         def on_draw():
-            self.on_draw_event_counter += 1
-            # clear surface
-            WINDOW.clear()
-            for batch in BATCHES:
-                BATCHES[batch].invalidate()
+            if self.on_draw_event_counter < MAXIMUM_DRAW_EVENTS_PER_FRAME:
+                self.on_draw_event_counter += 1
+                # clear surface
+                WINDOW.clear()
+                for batch in BATCHES:
+                    BATCHES[batch].invalidate()
 
-            # draw main batch: environment, main map, signals, trains
-            with MAP_CAMERA:
-                BATCHES['main_batch'].draw()
+                # draw main batch: environment, main map, signals, trains
+                with MAP_CAMERA:
+                    BATCHES['main_batch'].draw()
 
-            with UI_CAMERA:
-                # draw mini map batch: mini map
-                BATCHES['mini_map_batch'].draw()
-                # draw all vertices with shaders
-                self.app.on_apply_shaders_and_draw_vertices()
-                # draw ui batch: text labels, buttons
-                BATCHES['ui_batch'].draw()
+                with UI_CAMERA:
+                    # draw mini map batch: mini map
+                    BATCHES['mini_map_batch'].draw()
+                    # draw all vertices with shaders
+                    self.app.on_apply_shaders_and_draw_vertices()
+                    # draw ui batch: text labels, buttons
+                    BATCHES['ui_batch'].draw()
+
+                WINDOW.flip()
 
         @WINDOW.event
         def on_activate():
@@ -142,65 +191,20 @@ class RSSim:
             for h in self.app.on_resize_handlers:
                 h(width, height)
 
-        # determine if video adapter supports all game textures, if not - raise specific exception
-        max_texture_size = c_long(0)
-        gl.glGetIntegerv(gl.GL_MAX_TEXTURE_SIZE, max_texture_size)
-        if max_texture_size.value < REQUIRED_TEXTURE_SIZE:
-            raise VideoAdapterNotSupportedException
-
-        # determine if screen resolution meets requirements, if not - raise specific exception
-        if windll.user32.GetSystemMetrics(0) < MIN_RESOLUTION_WIDTH \
-                or windll.user32.GetSystemMetrics(1) < MIN_RESOLUTION_HEIGHT:
-            raise MonitorNotSupportedException
-
-        with open('db/config.db', 'rb') as f1, open('db/default.db', 'rb') as f2:
-            data = (f2.read() + f1.read())[::-1]
-            if sha512(data[::3] + data[1::3] + data[2::3]).hexdigest() != DATABASE_SHA512:
-                raise HackingDetectedException
-
-        with open(path.join(USER_DB_LOCATION, 'user.db'), 'rb') as f:
-            data = f.read()[::-1]
-            if sha512(data[::3] + data[1::3] + data[2::3]).hexdigest() \
-                    != get_password(sha512('user_db'.encode('utf-8')).hexdigest(),
-                                    sha512('user_db'.encode('utf-8')).hexdigest()):
-                raise HackingDetectedException
-
-        # check if game was updated from previous version (0.9.0 and higher are supported)
-        self.on_check_for_updates()
-        # set up the main logger, create log file
-        self.logger = getLogger('root')
-        current_datetime = datetime.now()
-        if not path.exists('logs'):
-            mkdir('logs')
-
-        logs_handler = FileHandler('logs/logs_{0}_{1:0>2}-{2:0>2}-{3:0>2}-{4:0>6}.log'
-                                   .format(str(current_datetime.date()), current_datetime.time().hour,
-                                           current_datetime.time().minute, current_datetime.time().second,
-                                           current_datetime.time().microsecond),
-                                   encoding='utf8')
-        logs_handler.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        self.logger.addHandler(logs_handler)
-        self.logger.setLevel(LOG_LEVEL_DEBUG)
-        # set blending mode; this is required to correctly draw transparent textures
-        gl.glEnable(gl.GL_BLEND)
-        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-        # create App object
-        self.app = AppController(loader=self)
-        clock.schedule_interval(on_app_update, 1/60)
-        # activate app after it is created
-        self.app.fade_in_animation.on_activate()
-        self.notifications = []
-        self.on_mouse_motion_event_counter = 0
-        self.on_mouse_motion_cached_movement = [0, 0]
-        self.on_mouse_drag_event_counter = 0
-        self.on_mouse_drag_cached_movement = [0, 0]
-        self.on_draw_event_counter = 0
-        self.on_mouse_scroll_event_counter = 0
-        self.on_mouse_scroll_cached_movement = [0, 0]
-
-    @staticmethod
-    def run():
-        app.run()
+    def run(self):
+        while True:
+            # dispatch_events() launches keyboard and mouse handlers implemented above
+            WINDOW.dispatch_events()
+            # increment in-game time
+            self.app.game.on_update_time()
+            # on_update_view() checks if all views content is up-to-date and opacity is correct
+            self.app.on_update_view()
+            # call on_draw() handler implemented above
+            WINDOW.dispatch_event('on_draw')
+            self.on_mouse_motion_event_counter = 0
+            self.on_mouse_drag_event_counter = 0
+            self.on_draw_event_counter = 0
+            self.on_mouse_scroll_event_counter = 0
 
     @staticmethod
     def on_check_for_updates():
