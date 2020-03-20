f11f791c85b053b1485f99a3f3c1671395e59d29
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2020-03-21 01:07:09 +0300

    trying to fix announcements

--- model/narrator_model/__init__.py
+++ model/narrator_model/__init__.py
@@ -8,25 +8,28 @@ from database import NARRATOR_QUEUE
 class NarratorModel(MapBaseModel, ABC):
     def __init__(self, controller, view, map_id):
         super().__init__(controller, view, map_id, logger=getLogger(f'root.app.game.map.{map_id}.narrator.model'))
-        self.narrator_queue = NARRATOR_QUEUE[self.map_id]
 
     @final
     def on_save_state(self):
         USER_DB_CURSOR.execute('''DELETE FROM narrator WHERE map_id = ?''', (self.map_id, ))
-        for announcement in self.narrator_queue:
+        for announcement in NARRATOR_QUEUE[self.map_id]:
             USER_DB_CURSOR.execute('''INSERT INTO narrator VALUES (?, ?, ?, ?, ?, ?)''', (self.map_id, *announcement))
 
     @final
     def on_update_time(self, dt):
+        NARRATOR_QUEUE[self.map_id] = sorted(NARRATOR_QUEUE[self.map_id], key=itemgetter(ANNOUNCEMENT_TIME))
+        self.logger.debug(f'{self.game_time=}')
+        self.logger.debug(f'{NARRATOR_QUEUE[self.map_id]=}')
+        while len(NARRATOR_QUEUE[self.map_id]) > 0 \
+                and self.game_time >= NARRATOR_QUEUE[self.map_id][0][ANNOUNCEMENT_TIME] \
+                and (NARRATOR_QUEUE[self.map_id][0][ANNOUNCEMENT_LOCKED] or not self.view.is_activated):
+            NARRATOR_QUEUE[self.map_id].pop(0)
+
         super().on_update_time(dt)
-        self.narrator_queue = sorted(self.narrator_queue, key=itemgetter(ANNOUNCEMENT_TIME))
-        while len(self.narrator_queue) > 0 and self.game_time >= self.narrator_queue[0][ANNOUNCEMENT_TIME] \
-                and (self.narrator_queue[0][ANNOUNCEMENT_LOCKED] or not self.view.is_activated):
-            self.narrator_queue.pop(0)
 
     @final
     def on_dt_multiplier_update(self, dt_multiplier):
-        for announcement in [announcement for announcement in self.narrator_queue
+        for announcement in [announcement for announcement in NARRATOR_QUEUE[self.map_id]
                              if announcement[ANNOUNCEMENT_TYPE]
                              in get_announcement_types_diff(self.dt_multiplier, dt_multiplier)]:
             announcement[ANNOUNCEMENT_LOCKED] = int(self.dt_multiplier > dt_multiplier)
@@ -37,7 +40,7 @@ class NarratorModel(MapBaseModel, ABC):
     def on_announcement_add(self, announcement_time, announcement_type, train_id, track_number):
         NARRATOR_QUEUE[self.map_id].append(
             (
-                announcement_time, not(announcement_type in get_announcement_types_enabled(self.dt_multiplier)),
+                announcement_time, int(announcement_type not in get_announcement_types_enabled(self.dt_multiplier)),
                 announcement_type, train_id, track_number
             )
         )
