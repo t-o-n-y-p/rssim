6d590e999b671cdb19432f4c2d9c88cfbe498c09
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2020-05-21 23:42:19 +0300

    implemented car collection rotation

--- model/map_model/freight_map_model.py
+++ model/map_model/freight_map_model.py
@@ -1,4 +1,3 @@
-from random import choice, seed
 from typing import final
 
 from model.map_model import MapModel
@@ -18,10 +17,10 @@ class FreightMapModel(MapModel):
         controller = FreightTrainController(self.controller, train_id)
         self.controller.fade_in_animation.train_fade_in_animations.append(controller.fade_in_animation)
         self.controller.fade_out_animation.train_fade_out_animations.append(controller.fade_out_animation)
-        seed()
         controller.model.on_train_init(
             cars, track, train_route, state, direction, new_direction, current_direction, priority, boarding_time,
-            exp, money, choice(self.unlocked_car_collections), switch_direction_required, self.exp_bonus_multiplier,
+            exp, money, self.unlocked_car_collections[0], switch_direction_required, self.exp_bonus_multiplier,
             self.money_bonus_multiplier, self.game_time, self.game_time_fraction, self.dt_multiplier
         )
+        self.unlocked_car_collections.append(self.unlocked_car_collections.pop(0))
         return controller
