cdecc2757ab5d97c4c65450252cb84a7e322e91e
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2020-06-01 01:18:47 +0300

    implementing UI v2

---
+++ ui/button_v2/previous_page_button_v2.py
@@ -0,0 +1,31 @@
+from logging import getLogger
+from typing import final
+
+from ui import get_top_bar_height
+from ui.button_v2 import UIButtonV2
+
+
+@final
+class PreviousPageButtonV2(UIButtonV2):
+    def __init__(self, on_click_action, parent_viewport):
+        super().__init__(logger=getLogger('root.button.previous_page_button'), parent_viewport=parent_viewport)
+        self.font_name = 'Arial'
+        self.on_click_action = on_click_action
+
+    def get_x(self):
+        return (self.parent_viewport.x1 + self.parent_viewport.x2) // 2 - get_top_bar_height(self.screen_resolution) * 5
+
+    def get_y(self):
+        return self.parent_viewport.y1
+
+    def get_font_size(self):
+        return int(32 / 80 * get_top_bar_height(self.screen_resolution))
+
+    def get_formatted_text(self):
+        return '◄'
+
+    def get_width(self):
+        return get_top_bar_height(self.screen_resolution)
+
+    def get_height(self):
+        return get_top_bar_height(self.screen_resolution)
---
+++ ui/button_v2/ru_locale_button_v2.py
@@ -0,0 +1,31 @@
+from logging import getLogger
+from typing import final
+
+from ui import get_top_bar_height
+from ui.button_v2 import UIButtonV2
+
+
+@final
+class RULocaleButtonV2(UIButtonV2):
+    def __init__(self, on_click_action, parent_viewport):
+        super().__init__(logger=getLogger('root.button.ru_locale_button'), parent_viewport=parent_viewport)
+        self.font_name = 'Arial'
+        self.on_click_action = on_click_action
+
+    def get_x(self):
+        return self.parent_viewport.x1 + get_top_bar_height(self.screen_resolution) - 2
+
+    def get_y(self):
+        return self.parent_viewport.y2 - get_top_bar_height(self.screen_resolution)
+
+    def get_font_size(self):
+        return int(38 / 80 * get_top_bar_height(self.screen_resolution))
+
+    def get_formatted_text(self):
+        return ''
+
+    def get_width(self):
+        return get_top_bar_height(self.screen_resolution)
+
+    def get_height(self):
+        return get_top_bar_height(self.screen_resolution)
